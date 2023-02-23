12434e78c85d941f8627a3e34a5ed32703b4a02f
zhu.wei1 <540201731@qq.com>
2023-02-23 15:56:35 +0800

    暗黑模式修改

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -26,7 +26,6 @@ const {isMenuCollapse} = toRefs(useMenusStore())
 <style scoped>
 .sumile-container {
   height: 100%;
-  background: var(--el-bg-color-overlay);
 }
 .sumile-main {
   padding: 15px 15px 15px 0;
